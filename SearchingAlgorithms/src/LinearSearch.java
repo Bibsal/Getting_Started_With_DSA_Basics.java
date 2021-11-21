bb3e5992ffc9333a0563c48a3cebde91e9d2f786
Bishal Keshri <bishalprasadkeshri@gmail.com>
2021-11-20 22:10:42 -0800

    Linear Search and Binary Search

---
+++ SearchingAlgorithms/src/LinearSearch.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class LinearSearch {
+	public static void main(String args[]) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = scn.nextInt();
+		}
+		
+		int dataToSearch = scn.nextInt();
+		int index = -1;
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] == dataToSearch) {
+				index = i;
+				System.out.print("Element found at index -->> ");
+				break;
+			}
+		}
+		
+		System.out.println(index);
+	}
+}

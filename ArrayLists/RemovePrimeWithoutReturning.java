9e0fcad565fa96ca877e97c5e48db876f1ffa168
Bishal Keshri <bishalprasadkeshri@gmail.com>
2021-11-26 23:03:06 -0800

    Update

--- ArrayLists/RemovePrimeWithoutReturning.java
+++ ArrayLists/RemovePrimeWithoutReturning.java
@@ -10,7 +10,7 @@ public class RemovePrimeWithoutReturning {
 			list.add(scn.nextInt());
 		}
 		removePrime(list);
-		System.out.print(list);  	//In previous removePrime program we directly called removePrime function in the print statement and also made a returnType ArrayList<Integer> and returned the list.
+		System.out.print(list);  		//In previous removePrime program we directly called removePrime function in the print statement and also made a returnType ArrayList<Integer> and returned the list.
 		// And here if we put the removePrime in the print statement then it'll give an error because the removePrime function isn't returning anything 
 		// (i.e return type void) so if it does not return anything then what will it print , so it gives an error , so we call it(the removePrime function) first and then print our arraylist 
 		// and in the previous code we directly called and printed it in the same print statement 

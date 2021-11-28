ab1308cd07b66dc5be1862920919af5aaeb5064f
Bishal Keshri <bishalprasadkeshri@gmail.com>
2021-11-27 23:22:47 -0800

    MinimumElementOfArray Using Recursion

---
+++ Practice/Recursion With Array/MinimumElementOfArray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class MinimumElementOfArray {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = scn.nextInt();
+		}
+		int minimum = MinimumElement(arr, 0);
+		System.out.println(minimum);
+	}
+	
+	public static int MinimumElement(int[] arr, int idx) {
+		if(idx == arr.length - 1) {
+			return arr[idx];
+		}
+		
+		int misa = MinimumElement(arr, idx + 1);
+		if(arr[idx] < misa) {
+			return arr[idx];
+		} else {
+			return misa;
+		}
+	}
+}

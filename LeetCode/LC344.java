f327b0dc859a72d0719ab4d70c191f262fd5ad36
Bibsal <bishalprasadkeshri@gmail.com>
2022-02-20 00:22:26 +0530

    lc 344

---
+++ LeetCode/LC344.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void reverseString(char[] s) {
+     
+        int last = s.length-1;
+        int first = 0;
+        
+        while(first< last)
+        {
+            char temp = s[first];
+            s[first] = s[last];
+            s[last] = temp;
+            first++;
+            last--;
+        }
+        
+    }
+}

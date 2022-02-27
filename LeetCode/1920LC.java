4d7666bece1fe8846d2613ab720f0c7e9e2a9c28
Bibsal <bishalprasadkeshri@gmail.com>
2022-02-27 17:22:45 +0530

    1920LC solved using the logic of inverse of array, just small changes

--- LeetCode/1920LC.java
+++ LeetCode/1920LC.java
@@ -4,7 +4,7 @@ class Solution {
         // Try following the same logic you used to solve the inverse question.
         int[] inv = new int[nums.length];
         for(int i = 0; i < nums.length; i++) {
-            int val = nums[nums[i]];
+            int val = nums[nums[i]];        // you can only write // int[i] = nums[nums[i]]; // and skip the below line
             inv[i] = val;
         }
         return inv;

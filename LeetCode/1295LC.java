c1383325023476244c2f81618649ef68b812dfde
Bibsal <bishalprasadkeshri@gmail.com>
2022-02-27 13:34:19 +0530

    Leetcode 1295 easy problem

--- LeetCode/1295LC.java
+++ LeetCode/1295LC.java
@@ -1,3 +1,4 @@
+// 1295. Find Numbers with Even Number of Digits
 class Solution {
     public int findNumbers(int[] nums) {
         int evenCount = 0;    // taking it so that if our nod is even we increase this to know that wo wala number even tha.

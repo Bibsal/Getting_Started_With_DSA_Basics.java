ef4e1cf21c5098bb3f868c22898f19e6d94cb175
Bibsal <bishalprasadkeshri@gmail.com>
2022-03-21 18:03:32 +0530

    GetStairPath clean code

--- Self-Practice/src/Recursion & BackTracking/RecursionInArrayList/GetStairPathBetterCode.java
+++ Self-Practice/src/Recursion & BackTracking/RecursionInArrayList/GetStairPathBetterCode.java
@@ -7,12 +7,12 @@ public class GetStairPathBetterCode {
     }
 
     public static ArrayList<String> getStairPath(int n) {
-        if(n == 0) {
+        if(n == 0) {                    // base case 1
             ArrayList<String> bres = new ArrayList<>();
             bres.add("");
             return bres;
         }
-        if(n < 0) return new ArrayList<>();
+        if(n < 0) return new ArrayList<>();     // base case 2
         ArrayList<String> myAnsArLst = new ArrayList<>();
         for(int jump = 1; jump <= 3; jump++) {
             ArrayList<String> faithpaths = getStairPath(n - jump);
@@ -21,6 +21,20 @@ public class GetStairPathBetterCode {
             }
         }
         return myAnsArLst;
+
+
+        /* We can even avoid our base case 2 and code in the following manner below
+            ArrayList<String> myAnsArLst = new ArrayList<>();
+            for(int jumps = 1; jumps <= 3; jumps++) {
+                if(n - jumps >= 0) {
+                    ArrayList<String> faithPaths = getStairPath(n - jumps);
+                    for(String ans : faithPaths) {
+                        myAnsArLst.add(jumps + ans);
+                    }
+                }
+            }
+            return myAnsArLst;
+        */
     }
     
 }

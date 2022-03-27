1ad78d29f7cc11f5ec726c7854073fe81ac917b9
Bibsal <bishalprasadkeshri@gmail.com>
2022-03-27 20:35:17 +0530

    PrintKPC

---
+++ Self-Practice/src/Recursion & BackTracking/Recursion On The Way Up/PrintKPC.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class PrintKPC {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        printKPC(str, "");
+    }
+    static String[] array = {".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
+
+    public static void printKPC(String str, String ans) {
+        if(str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+        String word = array[str.charAt(0) - '0'];
+        for(int i = 0; i < word.length(); i++) {
+            printKPC(str.substring(1), ans + word.charAt(i));
+        }
+    }
+}

21a1a2ae7ba1e43cb61b40421947dca58587a32e
Bibsal <bishalprasadkeshri@gmail.com>
2022-04-17 23:16:31 +0530

    Working with javascript

---
+++ Web/JavaScript FreeCodeCamp/gettingStarted.js
@@ -0,0 +1,22 @@
+// console.log("JavaScript prints from top to bottom & from left to right");
+console.log("Tu Hi Nirankar,\nMai Teri Saranha,\nMai Nu Bakshlo.");
+
+// JavaScript is a dynamically typed language, while Java is a static typed language.
+// It means ki JavaScript bolta hai ki ak variable declared hai, now you can input or assign any type of value to it, like let a = bishal, a = 10, so dono bishal and 10 same variable ko de skte hai, whereas 
+// Java ye bolta hai ki ak variable, eiis type ka declared hai, due to which you need to create separate variables for separate data type, you can't write int a = "bishal", because bishal is not an integer data type
+// JavaScript me by default, variables k andar 'undefined' store rhta hai
+
+
+let a;
+a = 10;       // integer
+
+a = 10.1;     // float
+
+a = true;     // boolean
+
+a = "hello, I am a string with double quotes and I can also be written inside single quotes like below";    // String
+a = 'this is also string, character data type nahi hota javascript me';
+
+a = null;
+
+console.log(a);                        // now since last updated/assigned value of a was null, so here null will be printed.

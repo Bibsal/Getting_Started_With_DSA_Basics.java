c978f3b93d589659c85313e98573e2361d53a96a
Bibsal <bishalprasadkeshri@gmail.com>
2022-04-26 01:36:48 +0530

    Created Tables

---
+++ Web/JavaScript FreeCodeCamp/sequel.sql
@@ -0,0 +1,26 @@
+CREATE TABLE employee (
+    -- CREATE TABLE  employee(
+    -- unique and Not Null
+--     EmpCode INT(4) ,
+--     EmpFName VARCHAR(40) NOT NULL,
+--     EmpLName VARCHAR(40) ,
+--     JOB VARCHAR(50) NOT NULL,
+--     -- correct char to int 
+--      ManagerCode  CHAR(4),
+--      HireDate    DATE NOT NULL,
+-- --    salary dataype add 
+--    Salary       INT(10) NOT NULL,
+--    Commission   INT(10),
+--    DEPTCODE     INT(3) NOT NULL
+-- --    email
+-- );
+    EmpCode Int(4) ,
+    EmpFName VARCHAR(40) NOT NULL,
+    EmpLName VARCHAR(40) ,
+    JOB VARCHAR(50) NOT NULL,
+    ManagerCode CHAR(4),  -- change this datatype from char to int using the alter command
+    HireDate DATE NOT NULL,
+    Salary INT(10) NOT NULL,
+    Commission INT(10),
+    DeptCode INT(3) NOT NULL
+);

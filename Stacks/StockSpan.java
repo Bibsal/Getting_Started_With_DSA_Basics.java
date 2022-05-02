4c3aeb122e4028ac09ca94d301fd6bd27aea3e02
Bibsal <bishalprasadkeshri@gmail.com>
2022-05-02 16:30:42 +0530

    Stock Span

---
+++ Stacks/StockSpan.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class StockSpan {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int[] ans = stockSpan(arr);
+        for(int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
+    }
+
+    public static int[] stockSpan(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int n = arr.length;
+        int[] span = new int[n];
+        for(int i = 0; i < span.length; i++) {
+            while(st.size() != 0 && arr[st.peek()] < arr[i]) {
+                st.pop();
+            }
+            span[i] = st.isEmpty() ? i + 1 : i - st.peek();
+            st.push(i);
+        }
+        return span;
+    }
+}

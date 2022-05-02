6d6a9a5019f70a8594efe6b05e07d0b60323bc20
Bibsal <bishalprasadkeshri@gmail.com>
2022-05-02 15:08:33 +0530

    Next Greater To Left

---
+++ Stacks/nextGreaterToLeft.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class nextGreaterToLeft {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int[] ans = ngleft(arr);
+        for(int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i]+" ");
+        }
+    }
+
+    public static int[] ngleft(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int n = arr.length;
+        int[] ans = new int[n];
+        for(int i = 0; i < n; i++) {
+            while(st.size() != 0 && st.peek() <= arr[i]) {
+                st.pop();
+            }
+            ans[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+}
